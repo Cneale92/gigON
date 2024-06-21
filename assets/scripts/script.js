342d0e9ce334326b8b4e149e85cfc960d06e0c2f
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-20 22:29:41 -0500

    Added pseudocode to script pages to plan out what they should look like

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+// set up spotify authorization
+
+// write a function to redirect the user to the Spotify login when they click the login button
+
+// use an event listener for the login button
+
+
