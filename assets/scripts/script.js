9a76c9d1e70ccfa0129e98b87d0418e08e89fcf1
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-06-20 22:43:43 -0500

    Set up script.js page, linked script to index.html page, made pseudocode instructions on the other js code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,15 @@
 // set up spotify authorization
+const spotifyClientID = `5e0f086d63214b34941f736646713e5c`;
+const spotifyRedirectUri = `Cneale92.github.io/gigON/stats.html`;
+const spotifyScopes = `user-top-read`; 
 
 // write a function to redirect the user to the Spotify login when they click the login button
-
+function redirectToSpotifyLogin() {
+    const authURL = `https://accounts.spotify.com/authorize?client_id=${spotifyClientId}&redirect_uri=${encodeURIComponent(spotifyRedirectUri)}&scope=${encodeURIComponent(spotifyScopes)}&response_type=token`;
+    window.location.href = authURL;    
+}
+ 
 // use an event listener for the login button
+document.querySelector('.button').addEventListener('click', redirectToSpotifyLogin);
 
 
