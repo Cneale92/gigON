b5a008fc1f5257d32c1b517670423a218e1187bf
Cneale92 <connorneale92@gmail.com>
2024-06-27 22:01:32 -0500

    added js to close modal on search

--- assets/scripts/results.js
+++ assets/scripts/results.js
@@ -29,6 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         .split(",")
         .map((item) => item.trim());
       fetchAndDisplayEvents(stateCountry, city);
+      locationModal.classList.add("is-active");
     } else {
       alert("Please enter a city, state, or country code.");
     }
